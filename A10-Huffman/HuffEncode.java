7f875e209bec5a1d048feb885d8fca8bc2f44ca0
cyang-05 <chiashiiyang@gmail.com>
2025-11-29 00:35:57 -0500

    huffencode done

--- A10-Huffman/HuffEncode.java
+++ A10-Huffman/HuffEncode.java
@@ -1,3 +1,107 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HuffEncode {
-    
+
+    /**
+     * hashmap that will hold character keys and its corresponding bit pattern
+     */
+    public static HashMap<Character, String> encodeMap = new HashMap<>();
+
+    /**
+     * creates a hashmap based off of characters and its corresponding ASCII values
+     * 
+     * @param filename, the filename of characters and its ASCII values
+     * @return nothing
+     */
+    public static void buildMap(String filename) {
+        Scanner file = null;
+        try {
+            file = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot locate file.");
+            // exits the system, flagging that program ended bcs of an error/exception
+            System.exit(-1);
+        }
+
+        // while the file has a line, go through the loop
+        while (file.hasNextLine()) {
+            // create a string line based off of next line in file
+            String line = file.nextLine();
+
+            // if the line is empty, continue
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            // create an array by splitting the line based off of spaces
+            String[] split = line.split("\\s+");
+
+            // stores the bit pattern (at index 0) as an string called pattern
+            String pattern = split[0];
+            // stores the ASCII value as an integer called ascii
+            int ascii = Integer.parseInt(split[1]);
+            // converts the ascii value into its corresponding character
+            char ch = (char) ascii;
+
+            // stores the character and its corresponding bit pattern value into the hashmap
+            encodeMap.put(ch, pattern);
+        }
+        file.close();
+
+    }
+
+    /**
+     * converts plain text to encoded text by using hashmap/hash table
+     * 
+     * @param filename, the file name containing text we want to encode
+     * @param table,    the hashmap/hashtable containing characters and its corresponding ASCII values
+     * @return the encoded text
+     */
+    public static String encodeText(String filename, Map<Character, String> table) {
+        Scanner file = null;
+        // will hold the ASCII values as an arraylist
+        ArrayList<String> encodeTextList = new ArrayList<String>();
+
+        // creates a scanner based off founded file
+        try {
+            file = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot locate file.");
+            // exits the system, flagging that program ended bcs of an error/exception
+            System.exit(-1);
+        }
+
+        // loops through each line in the file
+        while (file.hasNextLine()) {
+            // store the next line in attribute, "line"
+            String line = file.nextLine();
+            // loops through the string/line for each character
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+
+                // finds the character key in the hashmap (ascii)
+                // adds the key's value to the arraylist
+                encodeTextList.add(encodeMap.get(c));
+            }
+
+        }
+        file.close();
+
+        // converts the arraylist containing encoded text into a string
+        String encodedText = encodeTextList.toString();
+        System.out.println("This is the encoded text " + encodedText);
+        return encodedText;
+
+    }
+
+    public static void main(String[] args) {
+        buildMap("DefaultTree.txt");
+        encodeText("Original.txt", encodeMap);
+    }
+
 }
--- A10-Huffman/README.md
+++ A10-Huffman/README.md
@@ -3,12 +3,14 @@
 Your readme should include the following information. Each student needs to submit all of this information themselves, even when pair programming. If an item doesn't apply, please write N/A.
 
 ## General Information
-Programming Partner Name (if you did pair programming and would like to be graded together):
+Programming Partner Name (if you did pair programming and would like to be graded together): Chiashi Yang, Shakila Jafari, Kiara Sunil Singh, Victoria Reyes Borges
 
 Other Collaborators (and kudos to helpful members of the class):
 
-Any references used besides JavaDoc and course materials (including AI consultations):
+Any references used besides JavaDoc and course materials (including AI consultations): Geeks for Geeks
 
 ## Assignment Reflection
 
 Please reflect on your experience with this assignment. What was most challenging? What was most interesting?  What did you learn that you might use again?
+
+Chiashi Yang - I found the task of reading files and parsing the lines to be the most challenging. Although I have done this before for previous assignments, it was still difficult for me to figure out the logic. I had to use Geeks for Geeks to better understand what to do. I especially enjoyed coding the hash table containing the ASCII keys and the corresponding bit pattern values. I learned how to write code to encode plain text, and I also continued my learning in reading/parsing files. This will be helpful for future coding assignments and good knowledge overall. I wish the instructions were more clear, as I found it confusing that some methods' return values weren't specified. I had to infer on a lot of them. 
